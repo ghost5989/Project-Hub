59f16e81b860c851e63088eb212e4841c90b50a2
ghost5989 <ghost.shadow5989@gmail.com>
2026-09-17 00:24:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(helmet({
       "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       "font-src": ["'self'", "https://fonts.gstatic.com"],
       "img-src": ["'self'", "data:", "https:"],
-      "connect-src": ["'self'"],
+      "connect-src": ["'self'", "https://api.web3forms.com"],
       "frame-ancestors": ["'none'"]
     }
   },
